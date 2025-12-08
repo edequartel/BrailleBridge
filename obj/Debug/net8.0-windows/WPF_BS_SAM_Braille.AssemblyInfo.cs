2130ad6980cdf0e2c02ca7723bd9a470ebb1ce5a
Eric de Quartel <edequartel@bartimeus.nl>
2025-12-08 13:29:51 +0100

    update

--- obj/Debug/net8.0-windows/WPF_BS_SAM_Braille.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WPF_BS_SAM_Braille.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF_BS_SAM_Braille")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+911083605130a0e6ded6ab5183c3c5fe16119351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7134b95ceff30336cdbf0cd3742a642495efcfcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF_BS_SAM_Braille")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF_BS_SAM_Braille")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
